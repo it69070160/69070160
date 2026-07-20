8acdca244008f5bd4cd1be8e503f7e471c6d114b
it69070160 <69070160@kmitl.ac.th>
2026-07-21 00:19:19 +0700

    1412

---
+++ 3041.py
@@ -0,0 +1,11 @@
+"""3041"""
+def main():
+    """3041"""
+    number = int(input())
+    car = int(input())
+    new2 = number%car
+    if not new2:
+        print("yes")
+    else:
+        print("no")
+main()
